3b98a2cb8f303cd43aef2e195b97836246bf713e
LeaCasagrande <lea.casagrande27@gmail.com>
2023-02-09 16:41:59 +0100

    maj_inscription_back_2

--- mvc/controller/controller_signUp.php
+++ mvc/controller/controller_signUp.php
@@ -13,11 +13,12 @@ if(isset($_POST['pseudo_utilisateur']) && isset($_POST['mail_utilisateur']) && i
 
             //Extraction des données
             extract($_POST);
+            include('FUZZYTALES\mvc\model\set.php');
             ajoutUtilisateur($bdd,$pseudo_utilisateur,$mail_utilisateur,$mdp_utilisateur);
         }else{
             echo 'Les mots de passe ne sont pas identiques.';
         }
-        
+
     }
 
 }else{
