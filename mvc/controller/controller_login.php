837dfa59ac6c6c5c1c88d5b064c46c69e2b86e9a
LeaCasagrande <lea.casagrande27@gmail.com>
2023-02-10 15:37:39 +0100

    maj 10.02.2023

--- mvc/controller/controller_login.php
+++ mvc/controller/controller_login.php
@@ -1,13 +1,16 @@
 <?php
 // Connexion à la BDD
-include('FUZZYTALES\mvc\model\connect.php');
+include('../model/connect.php');
+
+// Affichage de la vue
+include('../../connect.php');
 
 //Test existence des champs
 if(isset($_POST['mail_utilisateur']) && isset($_POST['mdp_utilisateur'])){
 
     //Test champs remplis
     if(!empty($_POST['mail_utilisateur']) && !empty($_POST['mdp_utilisateur'])){
-
+        
     }
 }
 ?>
